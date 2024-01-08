393cb3600684bd514cba485e20c09f4da6665051
ujinthegrey <v4deus@gmail.com>
2024-01-08 22:06:31 +0400

    add btn interactivity

--- src/scripts/descriptions.js
+++ src/scripts/descriptions.js
@@ -3,5 +3,10 @@ const descriptions = document.querySelectorAll('.description')
 for (let i = 0; i < btns.length; i++ ) {
     btns[i].addEventListener('click', () => {
         descriptions[i].classList.toggle('hidden-description')
+        if (btns[i].innerHTML === 'Открыть описание') {
+            btns[i].innerHTML = 'Скрыть описание'
+        } else {
+            btns[i].innerHTML = 'Открыть описание'
+        }
     } )
 }
