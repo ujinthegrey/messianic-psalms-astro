9cb5f2f1996079c60efbbcf30625a47118d2c2ce
ujinthegrey <v4deus@gmail.com>
2024-01-30 14:36:59 +0400

    move from summary-description to button-script-description, add separations in description

--- src/scripts/descriptions.js
+++ src/scripts/descriptions.js
@@ -1,12 +1,12 @@
-const btns = document.querySelectorAll('.desctiption-btn')
+const btns = document.querySelectorAll('.description-btn')
 const descriptions = document.querySelectorAll('.description')
 for (let i = 0; i < btns.length; i++ ) {
     btns[i].addEventListener('click', () => {
         descriptions[i].classList.toggle('hidden-description')
-        if (btns[i].innerHTML === 'Открыть описание') {
-            btns[i].innerHTML = 'Скрыть описание'
+        if (btns[i].innerHTML === 'Описание') {
+            btns[i].innerHTML = 'Скрыть'
         } else {
-            btns[i].innerHTML = 'Открыть описание'
+            btns[i].innerHTML = 'Описание'
         }
     } )
 }
